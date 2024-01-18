1f88e6cf07b671d78cd79a20d65235f4b8d488c4
Mathieu MORGAT <morgatmathieu@gmail.com>
2024-01-18 13:53:31 +0100

    feat(axios): implemented a WIP axios for the backend

---
+++ src/axios/budgetAxios.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosInstance } from 'axios'
+
+const baseUrl = 'localhost:8000/api'
+
+const budgetAxios = axios.create({
+    baseURL: baseUrl,
+    timeout: 5000
+})
+
+/*
+
+//!NEEDS WORK
+
+Example request a mettre dans les views :
+
+import budgetAxios from '../axios/budgetAxios.ts'
+
+budgetAxios.get('/users').then(function (response){
+    console.log('Response : ', response.data)
+}).catch(function(error){
+    console.error('Error :', error)
+})
+
+*/
+
+export default budgetAxios
